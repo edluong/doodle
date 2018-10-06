699795eb89e005dfac7bb5de771c2150f6c54e31
edluong <edmundluong@gmail.com>
2018-10-05 22:17:33 -0700

    added poker hand posibilities to py file

--- cardsim/cards.py
+++ cardsim/cards.py
@@ -1,3 +1,5 @@
+from random import shuffle #needed to use the shuffle method for lists
+
 #set up the list of suits and ranks
 suits = ['Hearts','Diamonds','Clubs','Spades']
 rank = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
@@ -16,6 +18,17 @@ for card in deck:
     print(card,end="\n")
 
 #print the number of cards
-print('Number of cards:',len(deck))
+print('\nNumber of cards:',len(deck))
+
+#choose the top card
+shuffle(deck)
+print('Shuffling....')
+print('After shuffling, You picked:', deck[0][0],end='')
+print(' of',deck[0][1])
+
+print("Your Texas Hold'em Poker hand would be:",deck[:2])
+print("Your Omaha Poker hand would be:",deck[:4])
+print("Your Five Card Poker hand would be:",deck[:5])
+
 
     
